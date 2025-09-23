195b5da1583104f0facabcccaa9f239a99888b48
galymrasidovaa-ctrl <galymrasidovaa@gmail.com>
2025-09-23 15:58:42 +0500

    Лаб 1-2

---
+++ Aimira 23-14 IS.py
@@ -0,0 +1,34 @@
+a = 10
+b = 3
+result_subtraction = a - b
+print(f"Вычитание: {a} - {b} = {result_subtraction}")
+
+a = 5
+b = 7
+result_addition = a + b
+print(f"Сложение: {a} + {b} = {result_addition}")
+
+a = 4
+b = 6
+result_multiplication = a * b
+print(f"Умножение: {a} * {b} = {result_multiplication}")
+
+a = 2
+b = 3
+result_exponentiation = a ** b
+print(f"Возведение в степень: {a} ** {b} = {result_exponentiation}")
+
+a = 10
+b = 4
+result_division = a / b
+print(f"Деление: {a} / {b} = {result_division}")
+
+a = 10
+b = 3
+result_integer_division = a // b
+print(f"Целочисленное деление: {a} // {b} = {result_integer_division}")
+
+a = 10
+b = 3
+result_remainder = a % b
+print(f"Остаток от деления: {a} % {b} = {result_remainder}")
